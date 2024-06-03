33f478fc17115955e55bc431d90eee2a34bd2ddb
Sakshi <sakshiraut8802@gmail.com>
2024-06-03 16:52:46 +0530

    setting.py changed

--- movie_list/settings.py
+++ movie_list/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qd))l8s59nwh4cv44s0^ee5$0q&gc7_qx%0d=!ny0dou7k$=i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie-dump.netlify.app']
 
 
 # Application definition
